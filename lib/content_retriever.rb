caff79c0c3ed667de622d94896a598936e393054
alicyh <alicyh@gmail.com>
2014-02-08 12:06:39 +0800

    Update content_retriever.rb

--- lib/content_retriever.rb
+++ lib/content_retriever.rb
@@ -6,10 +6,10 @@ class ContentRetriver
 		{
 			total: 1,
 			contents: [{
-				title: '简单几步成为微信公众平台开发者',
-				description: '该公众账户是为了演示如何成为微信平台的开发者，如果想要了解更详细的内容，请点击该消息。',
-				picture: 'https://mp.weixin.qq.com/cgi-bin/getheadimg?fakeid=3097029212&r=975188&token=1685872554',
-				url: 'http://linkedme.in/2013/09/25/turoial-to-be-wechat-public-account-developer/'}]
+				title: '保卫菠萝2发布啦，赶快下载吧',
+				description: '保卫菠萝2是一个超萌的塔防游戏，推荐大家下载哦，苹果商店下载，点击马上~~~',
+				picture: 'http://tp4.sinaimg.cn/1923130343/180/40044228229/1',
+				url: 'https://itunes.apple.com/us/app/bao-wei-bo-luo2-chao-ji-chao/id790656910?ls=1&mt=8'}]
 		}
 	end
 end
